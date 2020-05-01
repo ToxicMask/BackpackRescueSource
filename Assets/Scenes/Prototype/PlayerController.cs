a26c612c1e827a54263bded3eb2550823cdf7e3e
ToxicMask <tomas.carciofi@gmail.com>
2020-05-01 19:29:35 -0300

    Basic Player

---
+++ Assets/Scenes/Prototype/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] PlayerMovement playerMove;
+    
+    void Start()
+    {
+        // Auto Get
+
+        playerMove = GetComponent<PlayerMovement>();
+    }
+    
+    void Update()
+    {
+
+        // Input
+        Vector2 walkDirection = new Vector2( Input.GetAxis("Horizontal"), 0 );
+
+        if (playerMove != null) playerMove.SetWalkDirection(walkDirection);
+        
+    }
+}
---
+++ Assets/Scenes/Prototype/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb2D;
+
+
+    // Input VAraibles
+    [SerializeField] Vector2 walkDirection = Vector2.zero;
+    [SerializeField] float walkSpeed = 1;
+
+   
+
+    // Standard Methods
+    void Start()
+    {
+        // Auto Get Components        
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    { 
+        // Walk
+        rb2D.velocity = (walkDirection * walkSpeed * Time.fixedDeltaTime);
+        
+    }
+
+    public void SetWalkDirection(Vector2 newVector)
+    {
+        //Debug.Log("ALERT: WALKING");
+        walkDirection = newVector;
+    }
+}
