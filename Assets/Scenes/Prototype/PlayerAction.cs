3bc5ce67e1ed5641d709ebbbe882246c1bc7872b
ToxicMask <tomas.carciofi@gmail.com>
2020-05-01 20:35:50 -0300

    Player Action Flow

---
+++ Assets/Scenes/Prototype/PlayerAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Try2PickObject()
+    {
+        Debug.Log("Picked");
+    }
+
+}
--- Assets/Scenes/Prototype/PlayerController.cs
+++ Assets/Scenes/Prototype/PlayerController.cs
@@ -6,16 +6,27 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] PlayerMovement playerMove;
+    [SerializeField] PlayerAction playerAction;
     
     void Start()
     {
         // Auto Get
 
         playerMove = GetComponent<PlayerMovement>();
+        playerAction = GetComponent<PlayerAction>();
     }
     
     void Update()
     {
+
+        if (Input.GetButtonDown("Action1"))
+        {
+            // Try to Pick Object
+            if (playerMove != null) playerAction.Try2PickObject();
+
+           return;
+        }
+
 
         // Input
         Vector2 walkDirection = new Vector2( Input.GetAxis("Horizontal"), 0 );
